ba29d5d0a21096719af78b2e82639c6583a2c7c1
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-10-23 09:24:03 +0200

    Created bin/template.py

---
+++ bin/ldisk.py
@@ -0,0 +1,19 @@
+"""ldisk
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-10-23"
+
+class ldisk:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(ldisk.ldisk())
+	
+	def ldisk():
+		"""ldisk first funtion
+		"""
+		return "ldisk" #for the test (see test_ldisk.py)
+		
+if __name__ == "__main__":
+	ldisk()
